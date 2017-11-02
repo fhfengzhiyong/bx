5ae2ebe06bae91db76fdcf5fd6bd153380e6cc7d
fhfengzhiyong <1107665984@qq.com>
2017-11-02 17:37:35 +0800

    add new ip

--- system/Db.go
+++ system/Db.go
@@ -8,12 +8,12 @@ import (
 var (
 	dbuser string = "root"
 	dbpwd  string = "1234"
-	dbname string = "bx"
+	dbname string = "104.128.82.190:3306/bx"
 )
 
 func init() {
-	conn := dbuser + ":" + dbpwd + "@/" + dbname + "?charset=utf8" //组合成连接串
-	orm.RegisterDriver("mysql", orm.DRMySQL)                       //注册mysql驱动
-	orm.RegisterDataBase("default", "mysql", conn)                 //设置conn中的数据库为默认使用数据库
-	orm.RunSyncdb("default", false, false)                         //后一个使用true会带上很多打印信息，数据库操作和建表操作的；第二个为true代表强制创建表
+	conn := dbuser + ":" + dbpwd + "@" + dbname + "?charset=utf8" //组合成连接串
+	orm.RegisterDriver("mysql", orm.DRMySQL)                      //注册mysql驱动
+	orm.RegisterDataBase("default", "mysql", conn)                //设置conn中的数据库为默认使用数据库
+	orm.RunSyncdb("default", false, false)                        //后一个使用true会带上很多打印信息，数据库操作和建表操作的；第二个为true代表强制创建表
 }
